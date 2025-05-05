e3b697f445ca744e815f7fb74b336e0021fcd3ed
Huang Chen Syuan <chensyuan.huang@gmail.com>
2025-05-04 23:14:35 -0500

    fix: 🐛 Change baseURL to variable for both prod and dev

--- src/pages/api/db/get_user.js
+++ src/pages/api/db/get_user.js
@@ -2,9 +2,11 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+const baseURL = process.env.BASE_URL
+
 export default async function handler(req, res) {
     // Set CORS headers (you can either use "*" for all origins or a specific one like below)
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.setHeader("Access-Control-Allow-Origin", baseURL);
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
     res.setHeader(
         "Access-Control-Allow-Headers",
