468cdf3b7ab29d9fc513ab30ddb1cf0508dc5604
DooHwanKim0419 <dkimh0419@gmail.com>
2025-05-12 18:04:24 -0500

    refactor: create game components

---
+++ src/components/Game/GameOver.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="text-center text-3xl font-bold pt-2 pb-1">Game Over</div>
+    <div class="text-center text-xl font-medium pt-2 pb-1">
+      Score: {{ score }} / 5
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  score: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
---
+++ src/components/Game/StartQuestionsButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button
+    @click="$emit('start-click')"
+    class="bg-[#087bb4] text-white font-bold py-2 px-4 rounded-lg shadow-md hover:bg-[#0d5f8b] mb-6"
+    :disabled="isIntroPlaying"
+    :class="{ 'opacity-50 cursor-not-allowed': isIntroPlaying }"
+  >
+    {{ isIntroPlaying ? 'Please wait...' : 'Start Questions' }}
+  </button>
+</template>
+
+<script setup>
+defineProps({
+  isIntroPlaying: {
+    type: Boolean,
+    required: true,
+  },
+});
+defineEmits(['start-click']);
+</script>
