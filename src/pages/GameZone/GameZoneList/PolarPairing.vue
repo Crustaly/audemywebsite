d4f86a9e1e145e544e423b89c02b7d47c390f46b
DooHwanKim0419 <dkimh0419@gmail.com>
2025-06-04 00:06:36 -0500

    refactor: updated polarpairing to properly handle async/await for audio playback

--- src/pages/GameZone/GameZoneList/PolarPairing.vue
+++ src/pages/GameZone/GameZoneList/PolarPairing.vue
@@ -120,8 +120,8 @@ const recordButtonClasses = computed(() => [
   isTablet.value
     ? 'w-[200px] h-[60px] pt-5 pr-[30px] pb-5 pl-[30px] gap-[10px] rounded-[20px]'
     : isMobile.value
-      ? 'w-full h-[60px] pt-5 pr-[30px] pb-5 pl-[30px] gap-[10px] rounded-[20px]'
-      : 'gap-2.5 w-[234px] h-[116px] pt-5 pr-7 pb-5 pl-7 rounded-[20px]',
+    ? 'w-full h-[60px] pt-5 pr-[30px] pb-5 pl-[30px] gap-[10px] rounded-[20px]'
+    : 'gap-2.5 w-[234px] h-[116px] pt-5 pr-7 pb-5 pl-7 rounded-[20px]',
   isRecording.value ? 'bg-red-500' : 'bg-[#087BB4]',
   'text-white',
   isButtonDisabled.value ? 'opacity-50 cursor-not-allowed' : '', // Apply cooldown/intro disable class
@@ -268,7 +268,7 @@ const repeatQuestion = () => {
 /**
  * Toggles the recording state when the record button is clicked
  */
-const toggleRecording = () => {
+const toggleRecording = async () => {
   if (isButtonDisabled.value) return;
 
   if (!isRecording.value) {
@@ -301,37 +301,28 @@ const toggleRecording = () => {
       if (userWords.some((word) => correctAnswers.includes(word))) {
         score.value++;
         console.log('Correct Answer!');
-        playSound('correctaudio.mp3');
+        await playSound('correctaudio.mp3');
       } else {
         console.log('Wrong Answer!');
         const incorrectAudio =
           'The correct answer is ' + currentQuestion.value['A'][0];
-        playSound('incorrectaudio.mp3');
+        await playSound('incorrectaudio.mp3');
 
-        setTimeout(() => {
-          currentAudios.push(playQuestion(incorrectAudio));
-        }, 1000);
+        await playQuestion(incorrectAudio);
       }
     }
 
+    transcription.value = '';
+
     numOfAudiosPlayed.value++;
 
-    setTimeout(() => {
-      transcription.value = '';
-      setTimeout(() => {
-        console.log('Recording processed and stopped. Cooldown ended.');
-        if (numOfAudiosPlayed.value < 5) {
-          setTimeout(() => {
-            playNextQuestion();
-          }, 500);
-        } else {
-          console.log('Game Over!');
-          setTimeout(() => {
-            playScore(score.value);
-          }, 1000);
-        }
-      }, 1500);
-    }, 500);
+    const isGameOver = numOfAudiosPlayed.value >= 5;
+
+    if (!isGameOver) {
+      playNextQuestion();
+    } else {
+      playScore(score.value);
+    }
   }
 };
 
