1997e14b34ac9fb09b42117019b38dfa23a20c04
Huang Chen Syuan <chensyuan.huang@gmail.com>
2025-06-10 10:54:48 -0500

    fix: 🐛 Replace custom CSS of loading UI

--- src/pages/LogIn/LogIn.vue
+++ src/pages/LogIn/LogIn.vue
@@ -1,7 +1,9 @@
 <template>
   <ScrollUpButton />
-  <div v-if="isLoading" class="loading-overlay">
-    <div class="spinner"></div>
+  <div
+    v-if="isLoading"
+    class="fixed inset-0 w-full h-full bg-white bg-opacity-80 flex flex-col justify-center items-center z-[9999]"
+  >
     <p>Loading...</p>
   </div>
   <div
@@ -472,20 +474,6 @@ input {
 #forgot-password-link {
   grid-column: 2;
   text-align: right;
-}
-
-.loading-overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(255, 255, 255, 0.8);
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  z-index: 9999;
 }
 
 /* * * * * Large Devices (≥1025px) * * * * */
