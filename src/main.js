a276ac168b9cdda634bf10f68e809475c9e9ecbd
master-of-none <shrikrishna.bht@gmail.com>
2025-06-12 18:54:15 +0530

    Updated the comment

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
-import { createApp } from "vue";
-import router from "./router/index.js";
-import App from "./App.vue";
-import "./style.css";
+import { createApp } from 'vue';
+import router from './router/index.js';
+import App from './App.vue';
+import './style.css';
 // import GoogleSignInPlugin from "vue3-google-signin";
-import vue3GoogleLogin from "vue3-google-login";
+import vue3GoogleLogin from 'vue3-google-login';
 
+// This will create the Vue application instance
 const app = createApp(App);
 
 app.use(vue3GoogleLogin, {
-    clientId: import.meta.env.VITE_GOOGLE_APP_CLIENT_ID, // Correct way with Vite
+  clientId: import.meta.env.VITE_GOOGLE_APP_CLIENT_ID, // Correct way with Vite
 });
 app.use(router);
-app.mount("#app");
+app.mount('#app');
