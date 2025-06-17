f07e2efd60b252dcf3cb73f6bd83daf0724b9413
DooHwanKim0419 <dkimh0419@gmail.com>
2025-06-17 16:52:29 -0500

    feat: prepare configs for colorgame, definitionDetective, divisionDuel

--- src/config/gameConfigs.js
+++ src/config/gameConfigs.js
@@ -9,4 +9,37 @@ export const gameConfigs = {
     dbKey: 'AdditionGame',
     category: 'math',
   },
+
+  colorGame: {
+    title: 'Color Game',
+    description: 'Name the color of a given object!',
+    bgColor: '#EACAFF',
+    iconSrc: '/assets/gameImages/buttons/gameButtons/color-game.svg',
+    introAudio: '/colorGame/colorIntro.mp3',
+    dbFile: 'crazyColorsDB.json',
+    dbKey: 'ColorQuizGame',
+    category: 'language',
+  },
+
+  definitionDetective: {
+    title: 'Definition Detective',
+    description: 'Find the correct word based on the definition.',
+    bgColor: '#9AE2EB',
+    iconSrc: '/assets/gameImages/buttons/gameButtons/definition.svg',
+    introAudio: '/definitionDetective/definitionIntro.mp3',
+    dbFile: 'definitionDetectiveDB.json',
+    dbKey: 'DefinitionDetectiveGame',
+    category: 'language',
+  },
+
+  division: {
+    title: 'Division Duel',
+    description: 'Divide the given numbers!',
+    bgColor: '#6FFFC6',
+    iconSrc: '/assets/gameImages/buttons/gameButtons/div.svg',
+    introAudio: '/divisionduel/divintro.mp3',
+    dbFile: 'divisionDb.json',
+    dbKey: 'DivisionGame',
+    category: 'math',
+  },
 };
