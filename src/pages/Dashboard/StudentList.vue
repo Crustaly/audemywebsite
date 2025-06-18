2496d04df5f15306888096334043d9c11c140a28
Ankita Shendge <ankitashendge15@gmail.com>
2025-06-17 23:03:30 -0700

    restored the file

---
+++ src/pages/Dashboard/StudentList.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1 class="text-2xl font-bold mb-4">Student</h1>
+    <p class="text-gray-700">
+      Here are the student details.
+    </p>
+  </div>
+</template>
+<script>
+export default { name: 'studentList' };
+</script>
