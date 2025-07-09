05a812d05c9396a7855a0c2a6ec28e2cdb5f87c8
Huang Chen Syuan <chensyuan.huang@gmail.com>
2025-07-09 08:58:24 -0500

    refactor: 💡 Updated tailwind for ResetLinkSent and ResetConfirm

--- src/pages/ForgotPassword/ResetLinkSent.vue
+++ src/pages/ForgotPassword/ResetLinkSent.vue
@@ -8,7 +8,6 @@ const { isMobile, isTablet } = useDeviceType();
 
 import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
-import Cookies from 'js-cookie';
 
 const userSession = ref(null);
 const showSchoolForm = ref(false); // Control form visibility
@@ -22,24 +21,12 @@ const forgotPassword = async (event) => {
 </script>
 
 <template>
-  <div
-    :class="[
-      'relative',
-      !isTablet && !isMobile ? 'px-14' : '',
-      isTablet ? 'px-6' : '',
-      isMobile ? 'px-8' : '',
-    ]"
-    ref="content"
-  >
+  <div class="page-container" ref="content">
     <Header :logoPath="'/assets/images/header/header-logo-2.png'" />
   </div>
   <div
     id="reset-link-sent-container"
-    :class="[
-      !isTablet && !isMobile ? 'px-20' : '',
-      isTablet ? 'px-10' : '',
-      isMobile ? 'px-5' : '',
-    ]"
+    class="content-container lg:grid lg:grid-cols-3"
   >
     <Banner
       id="reset-link-sent-banner"
@@ -48,29 +35,25 @@ const forgotPassword = async (event) => {
       bgColor="#B1C7D0"
       curveColor="#E5F0F5"
       :isPageShort="true"
+      class="lg:col-span-1 lg:h-full"
     />
     <!-- SHOW RESET FORM IF NOT LOGGED IN -->
     <div
       v-if="!userSession && !showSchoolForm"
       id="reset-link-sent-form-container"
-      class="pt-[20px] pb-[20px] mb-[40px] mt-[40px] text-center"
-      :class="[!isTablet && !isMobile ? 'mt-[0px] mb-[0px]' : '']"
+      class="form-container-view-height lg:col-span-2 lg:pb-[50px]"
     >
-      <h1 class="text-[#151E22] mobile:text-[28px] text-[35px]">Email Sent!</h1>
+      <h1 class="form-title">Email Sent!</h1>
       <br />
-      <p class="w-[80%] ml-[10%]">
+      <p class="form-description">
         Check your email and open the link we sent to continue.
       </p>
       <!-- FORGOT PASSWORD FORM -->
-      <form
-        @submit="forgotPassword"
-        method="post"
-        class="w-[80%] ml-[10%] mt-[20px] pt-[20px] pb-[20px]"
-      >
+      <form @submit="forgotPassword" method="post" class="form-wrapper">
         <div class="mt-[40px] mb-[40px] w-full">
           <button
             type="submit"
-            class="h-[55px] w-[280px] font-semibold text-white rounded-[8px] bg-[#087BB4] hover:bg-[#0C587D] hover:cursor-pointer border-2 border-black font-semibold shadow-[4px_4px_0px_black]"
+            class="primary-button font-semibold hover:bg-[#0C587D] hover:cursor-pointer"
           >
             Use another email
           </button>
@@ -81,24 +64,4 @@ const forgotPassword = async (event) => {
   <Footer />
 </template>
 
-<style scoped>
-/* * * * * Large Devices (≥1025px) * * * * */
-@media only screen and (min-width: 1025px) {
-  #reset-link-sent-container {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr); /* 3 equal columns */
-  }
-
-  #reset-link-sent-banner {
-    grid-area: 1 / span 1;
-    height: 100%;
-  }
-
-  #reset-link-sent-form-container {
-    margin-top: 0px;
-    margin-bottom: 0px;
-    grid-area: 1 / span 2;
-    padding-bottom: 50px;
-  }
-}
-</style>
+<style scoped></style>
--- src/pages/ResetPassword/ResetConfirm.vue
+++ src/pages/ResetPassword/ResetConfirm.vue
@@ -3,9 +3,6 @@ import Banner from '../../components/AccountPages/Banner.vue';
 import Header from '../../components/Header/Header.vue';
 import Footer from '../../components/Footer/Footer.vue';
 
-import { useDeviceType } from '../../Utilities/checkDeviceType';
-const { isMobile, isTablet } = useDeviceType();
-
 import { useRouter } from 'vue-router';
 
 const router = useRouter();
@@ -16,24 +13,12 @@ const toGameZone = () => {
 </script>
 
 <template>
-  <div
-    :class="[
-      'relative',
-      !isTablet && !isMobile ? 'px-14' : '',
-      isTablet ? 'px-6' : '',
-      isMobile ? 'px-8' : '',
-    ]"
-    ref="content"
-  >
+  <div class="page-container" ref="content">
     <Header :logoPath="'/assets/images/header/header-logo-2.png'" />
   </div>
   <div
     id="reset-confirm-container"
-    :class="[
-      !isTablet && !isMobile ? 'px-20' : '',
-      isTablet ? 'px-10' : '',
-      isMobile ? 'px-5' : '',
-    ]"
+    class="content-container lg:grid lg:grid-cols-3"
   >
     <Banner
       id="reset-confirm-banner"
@@ -42,29 +27,23 @@ const toGameZone = () => {
       bgColor="#B1C7D0"
       curveColor="#E5F0F5"
       :isPageShort="true"
+      class="lg:col-span-1 lg:h-full"
     />
     <div
       id="reset-confirm-form-container"
-      class="pt-[20px] pb-[20px] mb-[40px] mt-[40px] text-center"
-      :class="[!isTablet && !isMobile ? 'mt-[0px] mb-[0px]' : '']"
+      class="form-container-view-height lg:col-span-2 lg:pb-[50px]"
     >
-      <h1 class="text-[#151E22] mobile:text-[28px] text-[35px]">
-        You're all set!
-      </h1>
+      <h1 class="form-title">You're all set!</h1>
       <br />
-      <p class="w-[80%] ml-[10%]">
+      <p class="form-description">
         Use the password you just created the next time you log in.
       </p>
       <!-- GO TO GAME ZONE FORM -->
-      <form
-        @submit="toGameZone"
-        method="post"
-        class="w-[80%] ml-[10%] mt-[20px] pt-[20px] pb-[20px]"
-      >
-        <div class="mt-[40px] mb-[40px] w-full">
+      <form @submit="toGameZone" method="post" class="form-wrapper">
+        <div class="form-action-container">
           <button
             type="submit"
-            class="h-[55px] w-[280px] font-semibold text-white rounded-[8px] bg-[#087BB4] hover:bg-[#0C587D] hover:cursor-pointer border-2 border-black font-semibold shadow-[4px_4px_0px_black]"
+            class="primary-button font-semibold hover:bg-[#0C587D] hover:cursor-pointer"
           >
             Return to Game Zone
           </button>
@@ -74,24 +53,3 @@ const toGameZone = () => {
   </div>
   <Footer />
 </template>
-
-<style scoped>
-/* * * * * Large Devices (≥1025px) * * * * */
-@media only screen and (min-width: 1025px) {
-  #reset-confirm-container {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr); /* 3 equal columns */
-  }
-
-  #reset-confirm-banner {
-    grid-area: 1 / span 1;
-    height: 100%;
-  }
-
-  #reset-confirm-form-container {
-    margin-top: 0px;
-    margin-bottom: 0px;
-    grid-area: 1 / span 2;
-  }
-}
-</style>
