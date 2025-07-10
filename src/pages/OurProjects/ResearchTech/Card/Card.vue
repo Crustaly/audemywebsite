2baf764f3e63ce35d05a497b12d39b765e8f168d
Prajwala Shamnbulingappa <prajwalams99@gmail.com>
2025-07-10 16:01:12 -0400

    Updated the Our projects page

--- src/pages/OurProjects/ResearchTech/Card/Card.vue
+++ src/pages/OurProjects/ResearchTech/Card/Card.vue
@@ -11,20 +11,20 @@ const path = '/assets/images/our-projects/researchtech/';
 
 <template>
   <div class="w-full">
-    <div class="flex items-center w-full gap-[15px] py-4 lg:flex-col">
-      <div class="w-1/4 flex justify-center lg:w-full lg:justify-start">
+    <div class="flex flex-col items-center w-full gap-[15px] py-4 text-center">
+      <div class="flex justify-center">
         <div
           class="w-[76px] h-[76px] rounded-[8px] border-[1.5px] border-[#0C0D0D] shadow-[4px_4px_0px_#0C0D0D] flex justify-center items-center"
         >
           <img :src="path + icon" alt="icon" class="w-[90%] h-[90%]" />
         </div>
       </div>
-      <div class="w-3/4 flex flex-col font-poppins lg:w-full lg:justify-start">
-        <h2 class="text-2xl font-semibold">
+      <div class="flex flex-col font-poppins">
+        <h2 class="text-2xl font-semibold" v-if="title">
           {{ title }}
         </h2>
         <h4
-          class="text-left text-body-text-color max-w-[95%] text-[20px] font-[400]"
+          class="text-center text-body-text-color text-[20px] font-[400] whitespace-pre-line"
           v-html="text"
         ></h4>
       </div>
