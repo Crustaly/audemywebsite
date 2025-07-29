4d91984661ae2253b0a6ddaff352b0e68376f9fb
Prajwala Shamnbulingappa <prajwalams99@gmail.com>
2025-07-29 16:13:46 -0400

    Resolved all the merge conflicts

--- src/pages/Impact/CollaboratingSchools/Map.vue
+++ src/pages/Impact/CollaboratingSchools/Map.vue
@@ -25,16 +25,10 @@ const onMapIntersect = (entries) => {
 const loadMap = async () => {
   if (!window.tt) {
     // Load TomTom JS
-    await loadScript(
-      'https://api.tomtom.com/maps-sdk-for-web/cdn/5.x/5.64.0/maps/maps-web.min.js'
-    );
-    await loadScript(
-      'https://api.tomtom.com/maps-sdk-for-web/cdn/5.x/5.64.0/services/services-web.min.js'
-    );
+    await loadScript('https://api.tomtom.com/maps-sdk-for-web/cdn/5.x/5.64.0/maps/maps-web.min.js');
+    await loadScript('https://api.tomtom.com/maps-sdk-for-web/cdn/5.x/5.64.0/services/services-web.min.js');
     // Load CSS
-    loadCSS(
-      'https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.25.0/maps/maps.css'
-    );
+    loadCSS('https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.25.0/maps/maps.css');
   }
 
   const tt = window.tt;
@@ -73,85 +67,107 @@ const loadCSS = (href) => {
 const addMarkers = (map) => {
   const tt = window.tt;
   const locations = [
-    {
-      name: 'Alabama Institute for the Deaf and Blind',
-      coordinates: [-86.121667, 33.423889],
-    },
-    {
-      name: 'Arizona State School for the Deaf and the Blind',
-      coordinates: [-110.9904206, 32.2370163],
-    },
-    {
-      name: 'California School for the Blind',
-      coordinates: [-121.965, 37.561389],
-    },
-    {
-      name: 'Florida School for the Deaf and the Blind',
-      coordinates: [-81.31568, 29.91387],
-    },
-    {
-      name: 'Georgia Academy for the Blind',
-      coordinates: [-83.6686, 32.8481],
-    },
-    {
-      name: 'Hadley (IL)',
-      coordinates: [-87.7305, 42.1056],
-    },
-    {
-      name: 'Idaho Educational Services for the Deaf and the Blind',
-      coordinates: [-114.71018377937226, 42.930033266556364],
-    },
-    {
-      name: 'Illinois School for the Visually Impaired',
-      coordinates: [-90.21798754611584, 39.73565543633133],
-    },
-    {
-      name: 'Indiana School for the Blind and Visually Impaired',
-      coordinates: [-86.08515046145114, 39.83401516509762],
-    },
-    {
-      name: 'Iowa Educational Services for the Blind and Visually Impaired',
-      coordinates: [-95.82260501904825, 41.22588929543891],
-    },
-    {
-      name: 'Kansas State School for the Blind',
-      coordinates: [-94.6395, 39.11653],
-    },
-    {
-      name: 'Kentucky School for the Blind',
-      coordinates: [-85.713383, 38.255933],
-    },
-    {
-      name: 'Louisiana School for the Visually Impaired',
-      coordinates: [-91.188055, 30.393055],
-    },
-    {
-      name: 'Maryland School for the Blind',
-      coordinates: [-76.536111, 39.3675],
-    },
-    {
-      name: 'Missouri School for the Blind',
-      coordinates: [-90.245, 38.608333],
-    },
-    {
-      name: 'Perkins School for the Blind (MA)',
-      coordinates: [-71.1755, 42.362],
-    },
-    {
-      name: 'Vermont Association for the Blind and Visually Impaired',
-      coordinates: [-73.15, 44.466667],
-    },
-    {
-      name: 'Visually Impaired Preschool Services (KY + IN)',
-      coordinates: [-85.68384087502888, 38.20048802883136],
-    },
+  {
+            name: 'Alabama Institute for the Deaf and Blind',
+            coordinates: [-86.121667, 33.423889],
+          },
+          {
+            name: 'Arizona State School for the Deaf and the Blind',
+            coordinates: [-110.9904206, 32.2370163],
+          },
+          {
+            name: 'California School for the Blind',
+            coordinates: [-121.965, 37.561389],
+          },
+          {
+            name: 'Florida School for the Deaf and the Blind',
+            coordinates: [-81.31568, 29.91387],
+          },
+          {
+            name: 'Georgia Academy for the Blind',
+            coordinates: [-83.6686, 32.8481],
+          },
+          {
+            name: 'Hadley (IL)',
+            coordinates: [-87.7305, 42.1056],
+          },
+          {
+            name: 'Idaho Educational Services for the Deaf and the Blind',
+            coordinates: [-114.71018377937226, 42.930033266556364],
+          },
+          {
+            name: 'Illinois School for the Visually Impaired',
+            coordinates: [-90.21798754611584, 39.73565543633133],
+          },
+          {
+            name: 'Indiana School for the Blind and Visually Impaired',
+            coordinates: [-86.08515046145114, 39.83401516509762],
+          },
+          {
+            name: 'Iowa Educational Services for the Blind and Visually Impaired',
+            coordinates: [-95.82260501904825, 41.22588929543891],
+          },
+          {
+            name: 'Kansas State School for the Blind',
+            coordinates: [-94.6395, 39.11653],
+          },
+          {
+            name: 'Kentucky School for the Blind',
+            coordinates: [-85.713383, 38.255933],
+          },
+          {
+            name: 'Louisiana School for the Visually Impaired',
+            coordinates: [-91.188055, 30.393055],
+          },
+          {
+            name: 'Maryland School for the Blind',
+            coordinates: [-76.536111, 39.3675],
+          },
+          {
+            name: 'Missouri School for the Blind',
+            coordinates: [-90.245, 38.608333],
+          },
+          {
+            name: 'Perkins School for the Blind (MA)',
+            coordinates: [-71.1755, 42.362],
+          },
+          {
+            name: 'Vermont Association for the Blind and Visually Impaired',
+            coordinates: [-73.15, 44.466667],
+          },
+          {
+            name: 'Visually Impaired Preschool Services (KY + IN)',
+            coordinates: [-85.68384087502888, 38.20048802883136],
+          },
+          {
+            name: 'Texas School for the Blind and Visually Impaired (TSBVI)',
+            coordinates: [-97.738678, 30.314152],
+          },
+          {
+            name: 'St. Lucy\'s School for the Blind',
+            coordinates: [-75.031076, 40.055793,],
+          },
+          {
+            name: 'Massachusetts Commission for the Blind',
+            coordinates: [-71.0619767, 42.3528239],
+          },
+          {
+            name: 'The Chicago Lighthouse',
+            coordinates: [-87.672749, 41.867319],
+          },
+          {
+            name: 'Kansas State School for the Deaf and Blind (KSSDB)',
+            coordinates: [-94.812687, 38.882099],
+          },
+          {
+            name: 'Iowa Department for the Blind',
+            coordinates: [-93.6231159, 41.589356],
+          },
   ];
 
   locations.forEach((location) => {
     const marker = new tt.Marker().setLngLat(location.coordinates).addTo(map);
-    const popup = new tt.Popup({ offset: 25, closeButton: false }).setText(
-      location.name
-    );
+    const popup = new tt.Popup({ offset: 25, closeButton: false }).setText(location.name);
     marker.setPopup(popup);
   });
 };
