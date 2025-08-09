e7bb57405c978005b79e42d9680f3695caf031a3
Jennifer Chhie <22698132+jschhie@users.noreply.github.com>
2025-08-09 16:55:52 -0700

    feat: Add/use <PageDecorations/> in Access. Studio
    Changes:
    - Add reusable, custom UI component for maintainability
    - Organize stylesheet, adjust colors, & bolding in page content

---
+++ src/components/PageDecorations/PageDecorations.vue
@@ -0,0 +1,56 @@
+<script setup>
+/*
+ * PageDecorations.vue:
+ *
+ * - Decorates page content with four images, positioned at each corner (top/bottom/left/right)
+ * - This component is reusable, helping to keep templates DRY and maintainable
+ *
+ * Usage:
+ * - Its parent container must have `relative` positioning
+ * - May need to add padding to the parent container to prevent content from overlapping the images
+ * - Defaults to repeated star icons, if no image path is provided
+ */
+
+import Star from '/assets/images/studio/star.png';
+
+const props = defineProps({
+  topLeftImgPath: {
+    type: String,
+    required: false,
+  },
+  topRightImgPath: {
+    type: String,
+    required: false,
+  },
+  bottomLeftImgPath: {
+    type: String,
+    required: false,
+  },
+  bottomRightImgPath: {
+    type: String,
+    required: false,
+  },
+});
+</script>
+
+<template>
+  <!-- Decorative icons -->
+  <div aria-hidden="true">
+    <img
+      :src="topLeftImgPath ? topLeftImgPath : Star"
+      class="absolute top-5 left-5 w-[50px] rotate-[0.3rad]"
+    />
+    <img
+      :src="topRightImgPath ? topRightImgPath : Star"
+      class="absolute top-10 right-10 w-[40px] rotate-[0.45rad]"
+    />
+    <img
+      :src="bottomLeftImgPath ? bottomLeftImgPath : Star"
+      class="absolute bottom-5 left-5 w-[40px] rotate-[0.45rad]"
+    />
+    <img
+      :src="bottomRightImgPath ? bottomRightImgPath : Star"
+      class="absolute bottom-5 right-2 w-[45px] rotate-[0.3rad]"
+    />
+  </div>
+</template>
