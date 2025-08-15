7a414aff3e9e5774930e21d0e67e959d0f66d571
Jennifer Chhie <22698132+jschhie@users.noreply.github.com>
2025-08-15 13:32:03 -0700

    refactor: Replace JS device checks w/ Tailwind RWD
    - Replace 'checkDeviceType.js' import w/ native classes for performance
    - Adjust RWD margins & video scaling
    - Remove unused <ProjectsInAction />

--- src/pages/Impact/Impact.vue
+++ src/pages/Impact/Impact.vue
@@ -3,66 +3,52 @@
 import ScrollUpButton from '../../components/ScrollUpButton/ScrollUpButton.vue';
 import Header from '../../components/Header/Header.vue';
 import Footer from '../../components/Footer/Footer.vue';
+
+// Main content
 import Accolades from './Accolades/Accolades.vue';
 import OurReach from './OurReach/OurReach.vue';
 import CollaboratingSchools from './CollaboratingSchools/CollaboratingSchools.vue';
-import ProjectsInAction from '../OurProjects/ProjectsInAction/ProjectsInAction.vue';
 import ResearchTech from '../OurProjects/ResearchTech/ResearchTech.vue';
 import HaveYouHeard from '../OurProjects/Events/HaveYouHeard/HaveYouHeard.vue';
 import KatyYouthHacks from '../OurProjects/Events/KatyYouthHacks/KatyYouthHacks.vue';
 import SocialMedia from '../OurProjects/Social/SocialMedia.vue';
-import { useDeviceType } from '../../Utilities/checkDeviceType';
-
-const { isMobile, isTablet } = useDeviceType();
 </script>
 
 <template>
   <ScrollUpButton />
 
-  <div
-    :class="[
-      'relative',
-      !isTablet && !isMobile ? 'px-14' : '',
-      isTablet ? 'px-6' : '',
-      isMobile ? 'px-8' : '',
-    ]"
-    ref="content"
-  >
+  <div class="relative px-8 sm:px-8 md:px-6 lg:px-14 font-poppins">
     <Header :logoPath="'/assets/images/header/header-logo-2.png'" />
   </div>
 
-  <!--div class="flex justify-center px-8 md:px-14 xl:mt-20">
-    <div class="md:w-full md:mt-10">
-      <ProjectsInAction />
-    </div>
-  </div-->
-
-  <OurReach />
-
-  <CollaboratingSchools />
-  <Accolades />
-
-  <div class="flex justify-center px-8 md:px-14 mt-36">
-    <div class="md:w-full">
-      <ResearchTech />
-    </div>
+  <div class="relative w-[80%] md:w-full mx-auto font-poppins">
+    <OurReach />
   </div>
 
-  <div class="flex justify-center px-8 md:px-14 mt-36">
+  <CollaboratingSchools />
+
+  <div class="relative w-[80%] md:w-[90%] mx-auto py-10 font-poppins">
+    <Accolades />
+    <ResearchTech />
+  </div>
+
+  <div class="flex justify-center px-10 py-10 my-36">
     <div class="md:w-full">
       <HaveYouHeard />
     </div>
   </div>
-  <div class="flex justify-center px-8 md:px-14 mt-36">
+
+  <div class="flex justify-center mx-10 my-10">
     <div class="md:w-full">
       <KatyYouthHacks />
     </div>
   </div>
 
-  <div class="flex justify-center px-8 md:px-14 my-36">
-    <div class="md:w-full">
+  <div class="flex justify-center px-10 py-10 my-36">
+    <div class="md:w-[80%]">
       <SocialMedia />
     </div>
   </div>
+
   <Footer />
 </template>
