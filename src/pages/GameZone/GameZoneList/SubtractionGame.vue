699aeb67c1b7c8af620a77cde2f22d9009495266
Jennifer Chhie <22698132+jschhie@users.noreply.github.com>
2025-08-25 11:18:52 -0700

    feat: Captions for Math games Qs
    - Update <GameHeader/> props passed for each Math game
    - Games affected:
        - Fruit Frenzy, Shape Shark, Animal Add.,
        - Sub. Safari, Multi. Madness,
        - Division Duel, & Monkey Madness

--- src/pages/GameZone/GameZoneList/SubtractionGame.vue
+++ src/pages/GameZone/GameZoneList/SubtractionGame.vue
@@ -13,6 +13,9 @@
         :title="gameConfig.title"
         :description="gameConfig.description"
         :isMobile="isMobile"
+        :showQuestions="playButton && !isIntroPlaying && numOfAudiosPlayed < 5"
+        :numOfAudiosPlayed="numOfAudiosPlayed"
+        :currentQuestion="currentQuestion"
       />
 
       <PlayButton v-if="playButton === false" @play-click="playButton = true" />
