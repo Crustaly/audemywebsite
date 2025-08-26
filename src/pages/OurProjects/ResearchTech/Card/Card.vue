dabc76b8c3c8b0b3ca6838ced0fc545203e62415
Jennifer Chhie <22698132+jschhie@users.noreply.github.com>
2025-08-26 15:06:13 -0700

    refactor: RWD & DRY classes for <ResearchTech/> in Impact page
    - Replace icons & remove unused ones in <ResearchTech/>
    - Re-use shared Tailwind classes

--- src/pages/OurProjects/ResearchTech/Card/Card.vue
+++ src/pages/OurProjects/ResearchTech/Card/Card.vue
@@ -3,7 +3,6 @@ const props = defineProps({
   smallScreen: Boolean,
   icon: String,
   title: String,
-  text: String,
 });
 
 const path = '/assets/images/our-projects/researchtech/';
@@ -13,20 +12,14 @@ const path = '/assets/images/our-projects/researchtech/';
   <div class="w-full">
     <div class="flex flex-col items-center w-full gap-[15px] py-4 text-center">
       <div class="flex justify-center">
-        <div
-          class="w-[76px] h-[76px] rounded-[8px] border-[1.5px] border-[#0C0D0D] shadow-[4px_4px_0px_#0C0D0D] flex justify-center items-center"
-        >
-          <img :src="path + icon" alt="icon" class="w-[90%] h-[90%]" />
+        <div class="game-resource-icon-card relative">
+          <img :src="path + icon" alt="icon" class="w-[55px] absolute" />
         </div>
       </div>
-      <div class="flex flex-col font-poppins">
-        <h2 class="text-2xl font-semibold" v-if="title">
+      <div class="flex flex-col items-center font-poppins">
+        <h2 class="game-resource-card-caption-small text-center" v-if="title">
           {{ title }}
         </h2>
-        <h4
-          class="text-center text-body-text-color text-[20px] font-[400] whitespace-pre-line"
-          v-html="text"
-        ></h4>
       </div>
     </div>
   </div>
