153057d49d78949f46dfecf33bd2a675cf858411
Jennifer Chhie <22698132+jschhie@users.noreply.github.com>
2025-09-11 17:58:39 -0700

    feat: Hover effects to Game Page buttons
    - Improve UI consistency & UX; no logical changes
    
    Affected btns:
    - Play/Start Q, Go Back, Something Not Working,
    - Record/Stop, Repeat Q

--- src/components/Game/StartQuestionsButton.vue
+++ src/components/Game/StartQuestionsButton.vue
@@ -1,9 +1,11 @@
 <template>
   <button
     @click="$emit('start-click')"
-    class="page-button text-nowrap bg-[#087bb4] text-white px-10 w-full"
+    class="page-button text-nowrap bg-[#087BB4] text-white px-10 w-full"
     :disabled="isIntroPlaying"
-    :class="{ 'opacity-50 cursor-not-allowed': isIntroPlaying }"
+    :class="[
+      isIntroPlaying ? 'opacity-50 cursor-not-allowed' : 'hover:bg-[#0C587D]',
+    ]"
   >
     {{ isIntroPlaying ? 'Please wait...' : 'Start Questions' }}
   </button>
--- src/composables/useGameUI.js
+++ src/composables/useGameUI.js
@@ -20,7 +20,11 @@ export function useGameUI(gameState) {
         : 'w-[250px] h-[116px]',
     gameState.isRecording.value ? 'bg-red-500' : 'bg-[#087BB4]',
     'text-white',
-    isButtonDisabled.value ? 'opacity-50 cursor-not-allowed' : '',
+    isButtonDisabled.value
+      ? 'opacity-50 cursor-not-allowed'
+      : gameState.isRecording.value
+        ? 'hover:bg-red-700'
+        : 'hover:bg-[#0C587D]',
   ]);
 
   const recordButtonTitle = computed(() => {
