da6d51a845e8295ccf7e99bf23bc38852dfb1264
Jennifer Chhie <22698132+jschhie@users.noreply.github.com>
2025-09-20 15:24:48 -0700

    fix: RWD mobile/full images in Troubleshooting Page
    - Add RWD logic to switch between new mobile & full images
    - Rename full images for clarity & to support image RWD logic
    - Add 'prefix' & 'suffix' in .json for RWD image paths
    - Minor:
        - Add 'subtype' flag in .json for dynamic text styling
        - Update UI style for new tabs via openImageInNewTab()
        - Add clearer, updated images
        - Fix page text RWD

--- src/pages/Troubleshooting/Troubleshooting.vue
+++ src/pages/Troubleshooting/Troubleshooting.vue
@@ -7,9 +7,25 @@ import troubleshootingData from '../../assets/troubleshootingDB/troubleshooting.
 
 const openImageInNewTab = (imageSrc, imageName) => {
   const newTab = window.open();
-  newTab.document.body.innerHTML = `<img src="${imageSrc}" style="width:70%; height:auto;"/>`;
-  newTab.document.title = `image: ${imageName}`;
+  newTab.document.body.innerHTML = `
+    <div style="text-align:center; font-family:sans-serif; font-weight:normal;">
+      <h1 style="margin:2%;">${imageName}</h1>
+      <img src="${imageSrc}" style="width:80%; height:auto; margin:3%;"/>
+    </div>
+  `;
+  newTab.document.title = `${imageName}`;
 };
+
+// Shared RWD image classes
+const imageClasses = [
+  'w-[80%]',
+  'my-5',
+  'mx-auto',
+  'hover:cursor-pointer',
+  'rounded-lg',
+  'shadow-md',
+  'lg:shadow-none',
+];
 </script>
 
 <template>
@@ -20,7 +36,7 @@ const openImageInNewTab = (imageSrc, imageName) => {
   </div>
 
   <div
-    class="font-poppins relative flex flex-col items-start justify-center w-full px-10 my-16"
+    class="font-poppins relative flex flex-col items-start justify-center w-full my-16 text-center lg:text-left"
   >
     <!-- HEADER -->
     <div class="w-full">
@@ -28,22 +44,22 @@ const openImageInNewTab = (imageSrc, imageName) => {
     </div>
 
     <!-- QUESTIONS -->
-    <div class="flex flex-col w-full items-start justify-start my-3">
+    <div class="flex flex-col w-full items-center justify-center my-3">
       <!-- Loop through issues dynamically -->
       <div
         v-for="(issue, index) in troubleshootingData.issues"
         :key="index"
-        class="w-full md:w-[80%] mx-auto my-5 p-5 shadow-md rounded-lg bg-cross-lines"
+        class="w-[80%] mx-auto my-5 p-5 md:p-16 shadow-md rounded-lg bg-cross-lines"
       >
         <h3
-          class="p-3 text-[28px] md:text-[32px] font-semibold my-12 text-primary-color flex flex-col md:flex-row justify-center items-center text-center gap-5"
+          class="text-[28px] md:text-[32px] font-semibold text-primary-color flex flex-col md:flex-row justify-center lg:justify-start items-center gap-5"
         >
           <img
             src="/assets/images/troubleshooting/alert.png"
             aria-hidden="true"
             class="w-6 h-6"
           />
-          <p class="p-3 border-b-2 border-primary-color">{{ issue.name }}</p>
+          <p>{{ issue.name }}</p>
         </h3>
 
         <!-- Loop through the body items (titles, text, and images) -->
@@ -51,7 +67,7 @@ const openImageInNewTab = (imageSrc, imageName) => {
           <!-- Title -->
           <h4
             v-if="item.type === 'title'"
-            class="text-[18px] md:text-[20px] font-semibold my-5 text-center"
+            class="text-primary-color text-[18px] sm:text-[20px] md:text-[22px] font-semibold my-5"
           >
             <p>{{ item.content }}</p>
           </h4>
@@ -60,17 +76,42 @@ const openImageInNewTab = (imageSrc, imageName) => {
           <p
             v-if="item.type === 'text'"
             v-html="item.content"
-            class="page-text flex flex-col text-center"
+            :class="[
+              'page-text flex flex-col',
+              item.subtype === 'subheader'
+                ? 'border-b-2 p-3 border-primary-color font-semibold page-subheader'
+                : '',
+            ]"
           ></p>
 
-          <!-- Image -->
+          <!-- Mobile to Medium Screens: Display Cropped Image -->
           <img
             v-if="item.type === 'image'"
-            :src="item.path"
+            :src="item.prefix + 'mobile' + item.suffix"
             :alt="item.caption"
-            class="w-[504px] my-5 mx-auto hover:cursor-pointer"
+            :class="[imageClasses, 'block lg:hidden']"
             title="Open image in new tab"
-            @click="openImageInNewTab(item.path, item.caption)"
+            @click="
+              openImageInNewTab(
+                item.prefix + 'mobile' + item.suffix,
+                item.caption
+              )
+            "
+          />
+
+          <!-- Large+ Screens: Display Full Image -->
+          <img
+            v-if="item.type === 'image'"
+            :src="item.prefix + 'full' + item.suffix"
+            :alt="item.caption"
+            :class="[imageClasses, 'hidden lg:block']"
+            title="Open image in new tab"
+            @click="
+              openImageInNewTab(
+                item.prefix + 'full' + item.suffix,
+                item.caption
+              )
+            "
           />
         </div>
       </div>
