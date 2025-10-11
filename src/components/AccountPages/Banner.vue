abc3db7c95b2c013b7a461039d3ddb6a726ee5e2
Jennifer Chhie <22698132+jschhie@users.noreply.github.com>
2025-10-10 17:28:27 -0700

    refactor: Simplify Banner RWD w/ useDeviceDetection.js
    - Replace 'checkDeviceType.js' import w/ 'useDeviceDetection.js' to use 'isDesktop' flag

--- src/components/AccountPages/Banner.vue
+++ src/components/AccountPages/Banner.vue
@@ -7,8 +7,8 @@ import BlueStar from '/assets/images/about-us/blueStar2.svg';
 import Bulb from '/assets/images/about-us/bulb.png';
 import Glasses from '/assets/images/impact/glasses.svg';
 
-import { useDeviceType } from '../../Utilities/checkDeviceType';
-const { isMobile, isTablet } = useDeviceType();
+import { useDeviceDetection } from '../../composables/useDeviceDetection';
+const { isTablet, isMobile, isDesktop } = useDeviceDetection();
 
 // isImageWide: true if image width > height.
 // Helps Banner scale image: Reduces width for wide images, or height for tall ones.
@@ -64,7 +64,7 @@ const props = defineProps({
         class="absolute z-10"
         alt="orange star"
         :class="[
-          !isMobile && !isTablet ? 'w-[70px] right-[8%] top-[23%]' : '',
+          isDesktop ? 'w-[70px] right-[8%] top-[23%]' : '',
           isTablet ? 'w-[60px] right-[8%] top-[25%]' : '',
           isMobile ? 'w-[50px] right-[8%] top-[21%]' : '',
         ]"
@@ -75,7 +75,7 @@ const props = defineProps({
         class="absolute z-10"
         alt="yellow star"
         :class="[
-          !isMobile && !isTablet ? 'w-[65px] right-[40%] top-[15%]' : '',
+          isDesktop ? 'w-[65px] right-[40%] top-[15%]' : '',
           isTablet ? 'w-[60px] left-[75%] top-[20%]' : '',
           isMobile ? 'w-[50px] left-[70%] top-[20%]' : '',
         ]"
@@ -86,7 +86,7 @@ const props = defineProps({
         class="w-[65px] absolute z-10"
         alt="blue star"
         :class="[
-          !isMobile && !isTablet ? 'w-[70px] left-[8%] top-[22%]' : '',
+          isDesktop ? 'w-[70px] left-[8%] top-[22%]' : '',
           isTablet ? 'left-[21%] top-[20%]' : '',
           isMobile ? 'left-[18%] top-[20%]' : '',
         ]"
@@ -97,9 +97,9 @@ const props = defineProps({
         class="absolute z-10 rotate-[330deg]"
         alt="book"
         :class="[
-          !isMobile && !isTablet ? 'left-[40%] top-[75%] rotate-[360deg]' : '',
-          !isPageShort && !isMobile && !isTablet ? 'w-[60px]' : '',
-          isPageShort && !isMobile && !isTablet ? 'w-[50px]' : '',
+          isDesktop ? 'left-[40%] top-[75%] rotate-[360deg]' : '',
+          !isPageShort && isDesktop ? 'w-[60px]' : '',
+          isPageShort && isDesktop ? 'w-[50px]' : '',
           isTablet ? 'w-[50px] left-[12%] top-[60%]' : '',
           isMobile ? 'w-[50px] left-[10%] top-[53%]' : '',
         ]"
@@ -111,15 +111,15 @@ const props = defineProps({
         class="object-contain absolute left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%] z-10"
         alt="Carl icon"
         :class="[
-          !isMobile && !isTablet
+          isDesktop
             ? 'top-[40%] max-h-[80vw] max-w-[80vw/3]'
             : 'max-h-[200px] max-w-[80vw]',
 
-          !isPageShort && !isMobile && !isTablet ? 'w-[160px]' : '',
-          isPageShort && !isMobile && !isTablet ? 'w-[130px]' : '',
+          !isPageShort && isDesktop ? 'w-[160px]' : '',
+          isPageShort && isDesktop ? 'w-[130px]' : '',
 
-          (isTablet || isMobile) && isImageWide ? 'w-[150px]' : '',
-          (isTablet || isMobile) && !isImageWide ? 'h-[130px]' : '',
+          !isDesktop && isImageWide ? 'w-[150px]' : '',
+          !isDesktop && !isImageWide ? 'h-[130px]' : '',
         ]"
         v-if="CarlImgPath"
         loading="lazy"
@@ -129,9 +129,9 @@ const props = defineProps({
         class="absolute z-10 rotate-[20deg]"
         alt="lightbulb"
         :class="[
-          !isMobile && !isTablet ? 'right-[10%] top-[70%]' : '',
-          !isPageShort && !isMobile && !isTablet ? 'w-[55px]' : '',
-          isPageShort && !isMobile && !isTablet ? 'w-[50px]' : '',
+          isDesktop ? 'right-[10%] top-[70%]' : '',
+          !isPageShort && isDesktop ? 'w-[55px]' : '',
+          isPageShort && isDesktop ? 'w-[50px]' : '',
           isTablet ? 'w-[48px] right-[15%] top-[55%]' : '',
           isMobile ? 'w-[40px] right-[13%] top-[50%]' : '',
         ]"
@@ -142,16 +142,16 @@ const props = defineProps({
         class="absolute z-10"
         alt="glasses"
         :class="[
-          !isMobile && !isTablet ? 'left-[13%] top-[68%]' : '',
-          !isPageShort && !isMobile && !isTablet ? 'w-[60px]' : '',
-          isPageShort && !isMobile && !isTablet ? 'w-[50px]' : '',
+          isDesktop ? 'left-[13%] top-[68%]' : '',
+          !isPageShort && isDesktop ? 'w-[60px]' : '',
+          isPageShort && isDesktop ? 'w-[50px]' : '',
           isTablet ? 'w-[48px] left-[12%] top-[22%]' : '',
           isMobile ? 'w-[40px] left-[10%] top-[20%]' : '',
         ]"
         loading="lazy"
       />
     </div>
-    <div v-if="!isMobile && !isTablet" class="z-0">
+    <div v-if="isDesktop" class="z-0">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 1440 320"
