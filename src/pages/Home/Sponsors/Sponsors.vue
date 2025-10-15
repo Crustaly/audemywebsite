07f58dc87a29bdf9458d9412b54f302df78bb5d4
Jennifer Chhie <22698132+jschhie@users.noreply.github.com>
2025-10-14 21:18:41 -0700

    refactor: Add JSON DB for DRY dynamic <Sponsors/> UI
    - Create new 'sponsors.json' DB (for consistent <Sponsors/> & <SponsorsCarousel/> data)
    - Dynamically render responsive sponsor <img>'s using 'v-for' loop
    - Remove long, hardcoded, WET templates from <Sponsors/>

--- src/pages/Home/Sponsors/Sponsors.vue
+++ src/pages/Home/Sponsors/Sponsors.vue
@@ -1,80 +1,34 @@
 <script setup>
+import sponsorsData from '../../../assets/sponsorsDB/sponsors.json';
 import SponsorsCarousel from './SponsorsCarousel/SponsorsCarousel.vue';
 </script>
 
 <template>
   <div class="w-full my-32 mobile:my-24 flex flex-col gap-y-10 items-center">
     <h1 class="page-header-accent">SPONSORS THAT SUPPORT US, AMONG OTHERS:</h1>
-    <!-- Sponsors Carousel RWD Layout: 
-      - Mobile-first: 1 column 
+    <!-- Sponsors RWD Layout: 
+      - Mobile only: Hidden (Switches to <SponsorsCarousel/> UI)
       - Small: 2 cols
       - Medium: 3 cols
       - Large+ screens: 4 cols 
     -->
     <div class="hidden sm:block w-full">
       <div
-        class="w-full scroll-auto grid mobile:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-10 items-center justify-center text-center my-5"
+        class="w-full scroll-auto grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-10 items-center justify-center text-center my-5"
       >
-        <img
-          src="/assets/images/sponsors/intel.svg"
-          alt="intel logo"
-          class="w-1/2 md:w-[80%] mx-auto"
-        />
-        <img
-          src="/assets/images/sponsors/google.png"
-          alt="google logo"
-          class="w-1/2 md:w-[80%] mx-auto"
-        />
-        <img
-          src="/assets/images/sponsors/Amazon-Logo.png"
-          alt="amazon logo"
-          class="w-1/3 md:w-[50%] mx-auto"
-        />
-        <img
-          src="/assets/images/sponsors/ae.png"
-          alt="ae foundation logo"
-          class="w-1/3 md:w-[45%] mx-auto"
-        />
-        <img
-          src="/assets/images/sponsors/hersheys.svg"
-          alt="hersheys logo"
-          class="w-1/2 md:w-[80%] mx-auto"
-        />
-        <img
-          src="/assets/images/sponsors/desmos.png"
-          alt="desmos logo"
-          class="w-1/2 md:w-[80%] mx-auto"
-        />
-        <img
-          src="/assets/images/sponsors/aops.svg"
-          alt="aops logo"
-          class="w-1/2 md:w-[80%] mx-auto"
-        />
-        <img
-          src="/assets/images/sponsors/contribution.webp"
-          alt="contribution logo"
-          class="w-1/2 md:w-[70%] mx-auto"
-        />
-        <img
-          src="/assets/images/sponsors/tacobell.png"
-          alt="tacobell logo"
-          class="w-1/3 md:w-[70%] mx-auto"
-        />
-        <img
-          src="/assets/images/sponsors/wolframalpha.svg"
-          alt="wolframalpha logo"
-          class="w-1/2 md:w-[80%] mx-auto"
-        />
-        <img
-          src="/assets/images/sponsors/k4c-logo.svg"
-          alt="k4c logo"
-          class="w-1/3 md:w-[45%] mx-auto"
-        />
-        <img
-          src="/assets/images/sponsors/ncwit.png"
-          alt="ncwit logo"
-          class="w-1/3 md:w-[45%] h-auto mx-auto"
-        />
+        <!-- Loop through sponsors dynamically -->
+        <div v-for="(sponsor, index) in sponsorsData.sponsors" :key="index">
+          <img
+            :src="sponsor.src"
+            :alt="sponsor.alt"
+            :class="[
+              'h-auto mx-auto',
+              sponsor.isLogoWide === 'true'
+                ? 'w-1/3 md:w-[45%]'
+                : 'w-1/2 md:w-[80%]',
+            ]"
+          />
+        </div>
       </div>
     </div>
     <div class="block sm:hidden w-full">
