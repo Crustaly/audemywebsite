33e6cd2e40632528ec2b9e7d8bc587d28d9fdccc
Jennifer Chhie <22698132+jschhie@users.noreply.github.com>
2025-12-03 22:02:58 -0800

    refactor: +JS constant for Press List Card UI
    - Refactor long UI classes for code clarity

--- src/pages/Impact/PressList/PressListCard.vue
+++ src/pages/Impact/PressList/PressListCard.vue
@@ -6,12 +6,26 @@ const props = defineProps({
   url: String,
 });
 const path = '/assets/images/impact/';
+
+const cardClasses = [
+  'w-full', // RWD layout
+  'mx-auto',
+  'flex',
+  'flex-row',
+  'justify-center',
+  'p-2', // UI style
+  'rounded-[16px]',
+  'border',
+  'border-gray-400',
+  'hover:border-black', // Hover effects
+  'hover:shadow-[6px_6px_0px_#0C0D0D]',
+  'transition-all',
+  'duration-300',
+];
 </script>
 
 <template>
-  <div
-    class="w-full mx-auto flex flex-row justify-center p-2 rounded-[16px] border border-gray-400 hover:border-black hover:shadow-[6px_6px_0px_#0C0D0D] transition-all duration-300"
-  >
+  <div :class="cardClasses">
     <div class="w-full max-w-[360px] px-4">
       <div
         class="font-poppins flex flex-col items-center justify-evenly h-auto rounded-[8px] gap-4 mobile:px-4"
